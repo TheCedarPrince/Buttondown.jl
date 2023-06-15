a40deca1fd8bdb3eb2ad8cba3532c84506205012
Documenter.jl <documenter@juliadocs.github.io>
2023-06-15 15:22:45 +0000

    build based on be37f8b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Buttondown.jl-–-Basic-Julia-Wrapper-for-[Buttondown](https://buttondown.email/)","page":"Home","title":"Buttondown.jl – Basic Julia Wrapper for Buttondown","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Welcome to the Buttondown Julia API documentation!","category":"page"}]
+}
